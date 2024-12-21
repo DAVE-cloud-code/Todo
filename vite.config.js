b38e0c91bee4f7ee01866cbfce0ed5801e9a2e0e
DAVE-cloud-code <daviduche161@gmail.com>
2024-12-21 12:05:42 +0100

    tttt

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    host: '0.0.0.0', // Allows external access
+    port: 5001, // Change if needed
+  },
 })
+
